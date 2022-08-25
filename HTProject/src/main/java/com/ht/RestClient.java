30951b8b2c362e7cdc1ba9bf1f2b0d569d33c9f9
pbjena1995 <priyabratjena.95@gmail.com>
2022-08-25 12:23:38 +0530

    Day4

---
+++ HTProject/src/main/java/com/ht/RestClient.java
@@ -0,0 +1,42 @@
+package com.ht;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.ht.entity.User;
+
+public class RestClient {
+	
+	private static String url="http://localhost:8090/user";
+	
+	
+	public static void main(String[] args) {
+		//get();
+		post();
+	}
+	
+	private static void post() {
+		RestTemplate restTemp=new RestTemplate();
+		User user = new User();
+		user.setId(8);
+		user.setAge(23);
+		user.setName("BAC");
+		RestTemplate restTemplate =  new RestTemplate();
+		Object response = restTemplate.postForObject(url+"/save", user, Integer.class, new HashMap() );
+		System.out.println(response);
+
+	}
+
+	public static void get() {
+		RestTemplate restTemp=new RestTemplate();
+		ResponseEntity<List> forEntity = restTemp.getForEntity(url+"/get", List.class);
+		System.out.println(forEntity.getStatusCodeValue());
+		System.out.println(forEntity.getBody());
+	}
+
+}
